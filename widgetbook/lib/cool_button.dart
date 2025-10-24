3b416aa27829456596698ddd4a89e0d295f6bf1f
Jesutoni Aderibigbe <aderibigbejesutoni860@gmail.com>
2025-10-24 12:23:47 +0100

    feat: added coolbutton

---
+++ widgetbook/lib/cool_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+import 'package:techread_showcase/src/components/widgets/cool_button.dart';
+
+@widgetbook.UseCase(name: 'Cool Button', type: CoolButton)
+Widget buildCoolButtonUseCase(BuildContext context) {
+  return CoolButton(
+    label: context.knobs.string(label: 'Name', initialValue: 'Press Me'),
+    isFilled: context.knobs.boolean(label: 'Is Filled', initialValue: true),
+    onPressed: () {},
+  );
+}
